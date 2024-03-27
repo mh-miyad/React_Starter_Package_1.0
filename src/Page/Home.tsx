b4e40c7c6f48714c1cd9ca5f857565f73a4a832c
Mahamudul Hasan Miyad <mhmiyad6565@gmail.com>
2024-03-27 13:46:06 +0600

    Add Here

--- src/Page/Home.tsx
+++ src/Page/Home.tsx
@@ -1,19 +1,13 @@
-import "../style/Custom.css";
+import Header from "@/components/Header/Header";
+
 const Home = () => {
   return (
-    <div className={`pt-36 header-img`}>
-      <header>
-        <h1 className=" text-center mt-24  md:mt-32 font-dancing text-6xl md:text-7xl lg:text-8xl   tracking-wider leading-normal text-white font-medium px-5">
-          Enjoy In the best way{" "}
-        </h1>
-        <h3 className="text-center mt-4  text-2xl tracking-wider leading-normal text-white font-semibold px-5">
-          Enjoy our services for your trip anytime
-        </h3>
-      </header>
+    <>
+      <Header />
       <main>
         <h1>main part</h1>
       </main>
-    </div>
+    </>
   );
 };
 
